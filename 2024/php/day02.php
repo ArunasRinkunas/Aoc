eb8036d757644b6176855520c597e65d2939f946
Arunas Rinkunas <arunasrink.dev@gmail.com>
2024-12-02 17:51:49 +0200

    Day02 part 1 | PHP

---
+++ 2024/php/day02.php
@@ -0,0 +1,66 @@
+<?php
+
+$file = fopen("inputs/day02.txt", "r");
+if ($file) {
+    $count = 0;
+    $sum = 0;
+
+    while (($line = fgets($file)) !== false) {
+        $numbers = explode(' ', $line);
+        if (isLineNumbersIncreasing($numbers) || isLineNumbersDecreasing($numbers)) {
+            $count = checkIfNumberPairsDoNotExceed3($numbers);
+        }
+
+        $sum += $count;
+    }
+
+    print_r('Sum is: ' . $sum);
+}
+
+function isLineNumbersIncreasing(array $numbers): bool
+{
+    for ($i = 0; $i < count($numbers); $i++) {
+        if (
+            array_key_exists($i+1, $numbers)
+            && $numbers[$i] >= $numbers[$i+1]
+            
+            ) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isLineNumbersDecreasing(array $numbers): bool
+{
+    for ($i = 0; $i < count($numbers); $i++) {
+        if (
+            array_key_exists($i+1, $numbers)
+            && $numbers[$i] <= $numbers[$i+1]
+            
+            ) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function checkIfNumberPairsDoNotExceed3(array $numbers): int
+{
+    for ($i = 0; $i <= count($numbers); $i++) {
+        if (
+            array_key_exists($i+1, $numbers) 
+            && (
+                abs($numbers[$i] - $numbers[$i+1]) > 3
+            )
+        )
+        {
+            return 0;
+            break;
+        }
+    }
+
+    return 1;
+}
