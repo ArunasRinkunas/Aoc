e4d0d372c4d1f650f4aa1aa57d74a66610079d25
Arunas Rinkunas <arunasrink.dev@gmail.com>
2024-12-03 21:45:31 +0200

    Beautify code

--- 2024/php/day03.php
+++ 2024/php/day03.php
@@ -1,17 +1,26 @@
 <?php
 
+print_r('The answer is: ' . startProgram());
+
+function startProgram(): int
+{
+    preg_match_all('/mul\(([0-9]*[,][0-9]*)\)/', getCorruptedMemory(), $matches, PREG_UNMATCHED_AS_NULL);
+
+    return calculateMultiplications($matches);
+}
+
 function getCorruptedMemory() {
     return file_get_contents("inputs/day03.txt");
 }
 
-$corruptedMemory = getCorruptedMemory();
+function calculateMultiplications(array $matches): int
+{
+    $multipliedNumbers = [];
 
-$multipliedNumbers = [];
-preg_match_all('/mul\(([0-9]*[,][0-9]*)\)/', $corruptedMemory, $matches, PREG_UNMATCHED_AS_NULL);
+    foreach ($matches[1] as $multiplications) {
+        $explodedMultiplication = explode(',', $multiplications);
+        $multipliedNumbers[] = $explodedMultiplication[0] * $explodedMultiplication[1];
+    }
 
-foreach ($matches[1] as $multiplications) {
-    $explodedMultiplication = explode(',', $multiplications);
-    $multipliedNumbers[] = $explodedMultiplication[0] * $explodedMultiplication[1];
+    return array_sum($multipliedNumbers);
 }
-
-print_r(array_sum($multipliedNumbers));
--- 2024/php/day03_2.php
+++ 2024/php/day03_2.php
@@ -1,24 +1,22 @@
 <?php
 
+print_r('The answer is: ' . startProgram());
+
+function startProgram(): int
+{
+    $corruptedMemory = deleteEverythingBetweenDontAndDo(getCorruptedMemory());
+
+    preg_match_all('/mul\(([0-9]*[,][0-9]*)\)/', $corruptedMemory, $matches, PREG_UNMATCHED_AS_NULL);
+
+    return calculateMultiplications($matches);
+}
+
 function getCorruptedMemory() {
     return file_get_contents("inputs/day03.txt");
 }
 
-$corruptedMemory = getCorruptedMemory();
-
-$multipliedNumbers = [];
-$corruptedMemory = deleteEverythingBetweenDontAndDo($corruptedMemory);
-
-preg_match_all('/mul\(([0-9]*[,][0-9]*)\)/', $corruptedMemory, $matches, PREG_UNMATCHED_AS_NULL);
-
-foreach ($matches[1] as $multiplications) {
-    $explodedMultiplication = explode(',', $multiplications);
-    $multipliedNumbers[] = $explodedMultiplication[0] * $explodedMultiplication[1];
-}
-
-print_r(array_sum($multipliedNumbers));
-
-function deleteEverythingBetweenDontAndDo($corruptedMemory) {
+function deleteEverythingBetweenDontAndDo(string $corruptedMemory): string
+{
     preg_match_all('/don\'t\(\)(?:.|\n)*?do\(\)/', $corruptedMemory, $deleteMatches, PREG_UNMATCHED_AS_NULL);
 
     foreach ($deleteMatches[0] as $deleteMatch) {
@@ -26,4 +24,16 @@ function deleteEverythingBetweenDontAndDo($corruptedMemory) {
     }
 
     return $corruptedMemory;
+}
+
+function calculateMultiplications(array $matches): int
+{
+    $multipliedNumbers = [];
+
+    foreach ($matches[1] as $multiplications) {
+        $explodedMultiplication = explode(',', $multiplications);
+        $multipliedNumbers[] = $explodedMultiplication[0] * $explodedMultiplication[1];
+    }
+
+    return array_sum($multipliedNumbers);
 }
