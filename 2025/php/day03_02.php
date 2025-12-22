4f2cc80f03c0c0ac79d3cad1747299f043b3d02e
Arunas Rinkunas <arunasrink.dev@gmail.com>
2025-12-22 22:51:24 +0200

    Day 3 part 2

--- 2025/php/day03_02.php
+++ 2025/php/day03_02.php
@@ -3,50 +3,36 @@
 $file = fopen("inputs/day03.txt", "r");
 
 if ($file) {
-    $sumOfJolatages = 0;
+    $digitsLength = 12;
     $highestJoltages = [];
 
     while (($line = fgets($file)) !== false) {
         $line = rtrim($line);
-        // print_r($line . '<br>');
+
         $numbers = str_split($line);
-        // print_r(count($numbers) . '<br>');
-        $clonedNumbers = $numbers;
-        array_pop($clonedNumbers);
+        $numberCount = count($numbers);
 
-        // foreach ($clonedNumbers as $index => $slice) {
-        //     print_r('index: ' . $index . ' number: ' . $slice . '<br>');
-        // }
-        
-        $firstBiggestNumber = max($clonedNumbers);
-        // print_r('first biggest number: '. $firstBiggestNumber . '<br>');
-        $firstBiggestNumberIndex = array_search($firstBiggestNumber, $clonedNumbers);
-        // print_r('first biggest number index: '. $firstBiggestNumberIndex . '<br>');
+        $maximumJoltage = [];
+        for ($i = 0; $i < $numberCount; $i++) {
+            $number = $numbers[$i];
+            $remainingNumbers = $numberCount - $i;
 
-        $sliceArrayFromBiggestNumberIndex = array_slice($numbers, $firstBiggestNumberIndex + 1, null, true);
-        // print_r('the rest of array after slice: ' . '<br>');
-        
-        // foreach ($sliceArrayFromBiggestNumberIndex as $index => $slice) {
-        //     print_r('index: ' . $index . ' number: ' . $slice . '<br>');
-        // }
+            while (
+                !empty($maximumJoltage) &&
+                $maximumJoltage[count($maximumJoltage) - 1] < $number &&
+                (count($maximumJoltage) - 1 + $remainingNumbers) >= $digitsLength
+            ) {
+                array_pop($maximumJoltage);
+            }
 
-        $secondBiggestNumber = max($sliceArrayFromBiggestNumberIndex);
-        $secondBiggestNumberIndex = array_search($secondBiggestNumber, $numbers);
-        // print_r('first biggest number: '. $secondBiggestNumber . '<br>');
-        // print_r('first biggest number index: '.$secondBiggestNumberIndex . '<br>');
+            if (count($maximumJoltage) < $digitsLength) {
+                $maximumJoltage[] = $number;
+            }
+        }
 
-        // print_r('largest joltage possible: ' . $firstBiggestNumber . $secondBiggestNumber . '<hr>' . '<br>');
-        $highestJoltages[] = (int) ($firstBiggestNumber . $secondBiggestNumber);
-        
-        // die;
-        // foreach ($numbers as $index => $number) {
-        //     print_r('index: ' . $index . ' number: ' . $number . '<br>');
-        //     die;
-        // }
-        // die;
+        $highestJoltages[] = implode('', $maximumJoltage);
     }
 
-    // print_r($highestJoltages);
     print_r("Sum of all highest joltages: " . array_sum($highestJoltages));
     fclose($file);
 }
